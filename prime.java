a0d2c309dca1cbc37b9bf083e51d79c8be4d73c4
ummalarajunikhitha <ummalaraju77@gmail.com>
2025-11-09 12:03:02 +0530

    prime

---
+++ prime.java
@@ -0,0 +1,23 @@
+public class prime
+ {
+    public static void main(String[] args) 
+{
+        System.out.println("Prime numbers between 1 and 100 are:");
+        for (int num = 2; num <= 100; num++)
+ {
+            boolean isPrime = true;
+            for (int i = 2; i <= Math.sqrt(num); i++)
+ {
+                if (num % i == 0)
+ {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) 
+{
+                System.out.print(num + " ");
+            }
+        }
+    }
+}
